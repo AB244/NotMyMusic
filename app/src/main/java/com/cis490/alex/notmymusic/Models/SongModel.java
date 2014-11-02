936d2203657191a183fffa3d5a5dfda6c80a7f8a
AB244 <ajbenn04@louisville.edu>
2014-11-02 11:46:40 -0500

    Completed SongModel Class

---
+++ app/src/main/java/com/cis490/alex/notmymusic/Models/SongModel.java
@@ -0,0 +1,22 @@
+package com.cis490.alex.notmymusic.Models;
+
+/**
+ * Created by Alex on 11/2/2014.
+ */
+public class SongModel {
+
+    //Attributes
+    private int ID;
+    private String Title;
+    private String Artist;
+    private String Album;
+
+
+    public SongModel() {}
+
+    public SongModel(String title, String artist, String album ) {
+        this.Title = title;
+        this.Artist = artist;
+        this.Album = album;
+    }
+}
